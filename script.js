9456f29da8af9369a43b370a02931662d00b0114
leowader <101485176+leowader@users.noreply.github.com>
2024-09-21 16:12:26 -0500

    form

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const metaInfoArray = [
-      "Meta", "Información", "Blog", "Empleo", "Ayuda", "API", "Privacidad",
-      "Configuración de cookies", "Condiciones", "Ubicaciones", "Instagram Lite",
-      "Threads", "Importación de contactos y no usuarios", "Meta Verified",
-      "Español", "© 2024 Instagram from Meta"
-    ];
-  
-    // Añadir la información de meta dinámicamente
-    const metaContainer = document.querySelector('.flex.text-black');
-    metaInfoArray.forEach(info => {
-      const span = document.createElement('span');
-      span.textContent = info;
-      metaContainer.appendChild(span);
-    });
-  
-    // Manejo de envío del formulario
-    const form = document.getElementById('login-form');
-    form.addEventListener('submit', (event) => {
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const metaInfoArray = [
+    "Meta",
+    "Información",
+    "Blog",
+    "Empleo",
+    "Ayuda",
+    "API",
+    "Privacidad",
+    "Configuración de cookies",
+    "Condiciones",
+    "Ubicaciones",
+    "Instagram Lite",
+    "Threads",
+    "Importación de contactos y no usuarios",
+    "Meta Verified",
+    "Español",
+    "© 2024 Instagram from Meta",
+  ];
+
+  // Añadir la información de meta dinámicamente
+  const metaContainer = document.querySelector(".flex.text-black");
+  metaInfoArray.forEach((info) => {
+    const span = document.createElement("span");
+    span.textContent = info;
+    metaContainer.appendChild(span);
   });
-  
+
+  // Manejo de envío del formulario
+  const form = document.getElementById("login-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log("username:", username);
+    console.log("pass:", password);
+  });
+});
